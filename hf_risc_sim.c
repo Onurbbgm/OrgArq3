0b50230fa0d7a6a2bff449744d4f435b936b0b00
MarlonOrtiz23 <marlon.ortiz19@gmail.com>
2017-05-01 03:14:08 -0300

    Update hf_risc_sim.c

--- hf_risc_sim.c
+++ hf_risc_sim.c
@@ -58,9 +58,8 @@ int32_t log_enabled = 0;
 static int cache_read(uint32_t tag, uint32_t linha){
 	ciclos++;
 	acessosL1++;
-	int i = 0;
-	if(tagV[(linha+asso) % tam] == tag){
-			return (linha+asso) % tam;
+	if(tagV[linha] == tag){
+			return linha;
 	}
 	ciclos = ciclos + penal;
 	falhasL1++;
@@ -70,8 +69,8 @@ static int cache_read(uint32_t tag, uint32_t linha){
 static void cache_write(uint32_t tag, uint32_t linha, uint32_t data){
 	ciclos++;
 	acessosL1++;
-	cache[(linha+asso) % tam] = data;
-	tagV[(linha+asso) % tam] = tag;
+	cache[linha] = data;
+	tagV[linha] = tag;
 }
 
 static int32_t mem_read(state *s, int32_t size, uint32_t address){
