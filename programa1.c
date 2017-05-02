e7e61065080c047e95d244fbd376c79961619e19
MarlonOrtiz23 <marlon.ortiz19@gmail.com>
2017-05-01 21:30:59 -0300

    Update programa1.c

--- programa1.c
+++ programa1.c
@@ -12,25 +12,19 @@ int main(int argc , char * argv []){
 		lista[i] = rand()%100;	
 	}
 	//printf("%d\n",callQuick(lista));
-	printf("Lista nao organizada: \n");	
+	printf("\nLista nao organizada: ");	
 	int l;
 	for(l = 0; l<N; l++){
-		printf("%d\n",lista[l]);	
+		printf("%d, ",lista[l]);	
 	}
 	quickSort(lista, 0, 99);
-	printf("Lista organizada: \n");	
+	printf("\nLista organizada: ");	
 	int j;
 	for(j = 0; j<N; j++){
-		printf("%d\n",lista[j]);	
+		printf("%d, ",lista[j]);	
 	}
 	return 0;
 }
-
-//int callQuick(int vetor[]){
-//	int len = sizeof(vetor)/sizeof(vet[0]);
-//	return len;
-	//quickSort(vetor, 0, len-1);
-//}
 
 void quickSort(int vetor[], int comeco, int fim){
     int pivot, aux;
