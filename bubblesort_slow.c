f6611bf11c110ddc3b4b4506db095630bc467762
MarlonOrtiz23 <marlon.ortiz19@gmail.com>
2017-05-02 17:37:45 -0300

    Add files via upload

---
+++ bubblesort_slow.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//#include <hf-risc.h>
+
+#define N 100
+
+void bubble(int lista[100]);
+
+int main(int argc , char * argv []){
+        int i;
+        int lista[100];
+        for(i = 0; i<N; i++){
+                lista[i] = random()%100;
+        }
+        printf("\nLista nao organizada: ");
+        int l;
+        for(l = 0; l<N; l++){
+                printf("%d, ",lista[l]);
+        }
+        bubble(lista);
+        printf("\nLista organizada: ");
+        int j;
+        for(j = 0; j<N; j++){
+                printf("%d, ",lista[j]);
+        }
+	printf("\n");
+        return 0;
+}
+
+void bubble(int lista[100]){
+	int i,j,aux;
+  	for (i = 0 ; i < 99; i++)
+  	{
+    		for (j = 99-i ; j > 0; j--)
+		{
+			if (lista[j] > lista[j+1])
+			{
+		        	aux       = lista[j];
+		        	lista[j]   = lista[j+1];
+		        	lista[j+1] = aux;
+			}
+		}
+	}
+}
---
+++ quicksort_fast.c
@@ -0,0 +1,53 @@
+#include <hf-risc.h>
+
+void quickSort(int lista[100], int comeco, int fim);
+#define N 100
+int main(int argc , char * argv []){
+	int i;
+	int lista[100];
+	for(i = 0; i<N; i++){
+		lista[i] = random()%100;
+	}
+	printf("\nLista nao organizada: ");
+	int l;
+	for(l = 0; l<N; l++){
+		printf("%d, ",lista[l]);
+	}
+	quickSort(lista, 0, 99);
+	printf("\nLista organizada: ");
+	int j;
+	for(j = 0; j<N; j++){
+		printf("%d, ",lista[j]);
+	}
+	return 0;
+}
+
+void quickSort(int vetor[], int comeco, int fim){
+    int pivot, aux;
+	int i, j;
+
+	pivot = vetor[(comeco+fim)/2];
+	i = comeco;
+	j = fim;
+
+	while(i <= j){
+		while(vetor[i] < pivot)
+			i++;
+
+		while(vetor[j] > pivot)
+			j--;
+
+		if(i <= j){
+			aux = vetor[i];
+			vetor[i] = vetor[j];
+			vetor[j] = aux;
+			i++;j--;
+       		 }
+	}
+
+	if(j > comeco)
+		quickSort(vetor, comeco, j);
+
+	if(i < fim)
+		quickSort(vetor, j+1, fim);
+}
